72182d211d6239981f08e150404539243b032921
cathtayer <149668326+cathtayer@users.noreply.github.com>
2025-10-30 00:37:31 +0800

    Update book.dart

--- book_list/lib/models/book.dart
+++ book_list/lib/models/book.dart
@@ -14,15 +14,39 @@ class Book {
 
 final List<Book> books = [
   Book(
-    title: "Attached",
-    author: "Amir Levine & Rachel Heller",
-    genre: "Psychology",
-    summary: "A deep dive into attachment styles and how they affect relationships.",
+    title: "Nineteen Eighty-Four",
+    author: "George Orwell",
+    genre: "Dystopian",
+    summary: "A chilling novel about a society under total surveillance, where the government controls truth and individuality.",
   ),
   Book(
     title: "Dracula",
     author: "Bram Stoker",
     genre: "Gothic Fiction",
     summary: "A chilling tale of the infamous vampire Count Dracula.",
+  ),
+  Book(
+    title: "The Memoirs of Sherlock Holmes",
+    author: "Arthur Conan Doyle",
+    genre: "Detective Fiction",
+    summary: "A collection of thrilling cases showcasing Sherlock Holmes’ sharp intellect and unmatched deduction skills.",
+  ),
+    Book(
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Southern Gothic / Coming-of-Age",
+    summary: "A powerful story of racial injustice and childhood innocence in the American South.",
+  ),
+  Book(
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    genre: "Psychological Fiction / Philosophical Novel",
+    summary: "A gripping exploration of guilt, morality, and the tortured mind of a man who commits murder.",
+  ),
+  Book(
+    title: "A Random Walk Down Wall Street",
+    author: "Burton G. Malkiel",
+    genre: "Finance / Investment",
+    summary: "A guide to understanding the stock market and long-term investing through the 'random walk' theory.",
   ),
 ];
