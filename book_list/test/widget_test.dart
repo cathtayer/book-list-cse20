4a789a52dc2970bcea53113e52a67f5d64b61584
cathtayer <149668326+cathtayer@users.noreply.github.com>
2025-10-30 00:15:10 +0800

    .

--- book_list/test/widget_test.dart
+++ book_list/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:book_liat/main.dart';
+import 'package:book_list/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
